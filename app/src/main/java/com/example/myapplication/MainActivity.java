e97a67ffb0a7618e9bd1df29c0848fd22516a790
cathy192 <korea@2047>
2020-04-12 14:41:20 +0900

    Make alarm funtion

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +24,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
+import static com.example.myapplication.subActivity2.mcontext;
+import static com.example.myapplication.subActivity2.stateFunction;
+
 public class MainActivity extends AppCompatActivity {
     Button btn1;
     Button btn2;
@@ -26,14 +37,27 @@ public class MainActivity extends AppCompatActivity {
 
     TextView time;
     final Handler timeHandler = new Handler();
+
+    NotificationManager manager; NotificationCompat.Builder builder; private static String CHANNEL_ID = "channel1"; private static String CHANEL_NAME = "Channel1";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.InitializeView();
         this.SetListener();
+        if(
+                ((subActivity2)mcontext).stateFunction()=="yellow")
+        {
+        this.showNoti();}
+        //알람 울리기
 
-    }
+
+
+
+
+        }
+
 
     public void InitializeView() {
         btn1 = (Button) findViewById(R.id.button1);
@@ -45,31 +69,42 @@ public class MainActivity extends AppCompatActivity {
 
       //  final TextView time = (TextView) findViewById(R.id.DayTimer);
     }
-        long now = System.currentTimeMillis();
-        SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        Date mdate = new Date(now);
-        String mdataformate = simpleDate.format(mdate);
-        //time.setText(mdataformate);
-
-
-
-        protected void update(){
-            Runnable updater = new Runnable() {
-                @Override
-                public void run() {
-                    Log.d("timeTimerTask", "Time : " + mdataformate);
-                    time.setText(mdataformate);
-                }
-            };
-
-            timeHandler.post(updater);
-        }
 
 
 
 
+    public void showNoti(){
+        PendingIntent mpendingIntent = PendingIntent.getActivity(
+                MainActivity.this,0,
+                new Intent(getApplicationContext(),MainActivity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        builder = null;
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE); //버전 오레오 이상일 경우
+         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+             manager.createNotificationChannel(
+                     new NotificationChannel(CHANNEL_ID, CHANEL_NAME, NotificationManager.IMPORTANCE_DEFAULT));
+             builder = new NotificationCompat.Builder(this, CHANNEL_ID); //하위 버전일 경우
+
+         }else{
+        builder = new NotificationCompat.Builder(this);
+         } //알림창 제목
+         builder.setContentTitle("IOT화재감지기"); //알림창 메시지
+         builder.setContentText("현재 상태 위험"); //알림창 아이콘
+        builder.setContentIntent(mpendingIntent);
+      //  builder.setAutoCancel(true);//누르면 알림 삭제
+         builder.setSmallIcon(R.drawable.check);
+         Notification notification = builder.build(); //알림창 실행
+         manager.notify(1,notification);
+    }
 
 
+
+
+
+
+
+//버튼 눌러 다른 페이지로 이동
     public void SetListener() {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,7 +112,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "버튼 테스트", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(
                         getApplicationContext(), subActivity1.class);
-                Log.v("테스트", "버튼");
 
 
                 startActivity(intent);
@@ -87,17 +121,20 @@ public class MainActivity extends AppCompatActivity {
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.v("테스트", "버튼");
-                Toast.makeText(getApplicationContext(),"버튼 테스트",Toast.LENGTH_LONG).show();
-                Intent intent2= new Intent(getApplicationContext(), subActivity2.class);
+
+                //   Toast.makeText(getApplicationContext(),"버튼 테스트",Toast.LENGTH_LONG).show();
+                Intent intent2 = new Intent(getApplicationContext(), subActivity2.class);
                 startActivity(intent2);
+
+
+
             }
         });
 
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.v("테스트", "버튼");
+
                 Toast.makeText(getApplicationContext(),"버튼 테스트",Toast.LENGTH_LONG).show();
                 Intent intent3= new Intent(getApplicationContext(), subActivity3.class);
                 startActivity(intent3);
@@ -115,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.v("테스트", "버튼");
+
                 Toast.makeText(getApplicationContext(),"버튼 테스트",Toast.LENGTH_LONG).show();
                 Intent intent5= new Intent(getApplicationContext(), subActivity5.class);
                 startActivity(intent5);
@@ -124,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.v("테스트", "버튼");
+
                 Toast.makeText(getApplicationContext(),"버튼 테스트",Toast.LENGTH_LONG).show();
                 Intent intent6= new Intent(getApplicationContext(), subActivity6.class);
                 startActivity(intent6);
--- app/src/main/java/com/example/myapplication/subActivity2.java
+++ app/src/main/java/com/example/myapplication/subActivity2.java
@@ -1,14 +1,36 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class subActivity2 extends AppCompatActivity {
+    public static Context mcontext;
+    static int a=30;//센서1, 추후 업데이트
+    public static  String stateFunction(){//센서 값과 기준값 비교
 
+        String state="0";// 현재 센서 값에 따른 현재 상황
+        if(a<=20){
+            state="green";
+        }
+        else if(a>20 && a<=40){
+            state="yellow";
+        }
+        else if(a>40){
+            state="red";
+        }
+        return state;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub2);
+        mcontext=this;
     }
+
+
+
+
+
 }
