014b2f18503da92131897826b8144277f21cd2ed
y7133 <41673178+y7133@users.noreply.github.com>
2020-06-01 03:08:30 +0900

    Update COgas.py

--- COgas.py
+++ COgas.py
@@ -1,8 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 import urllib
-# change these as desired - they're the pins connected from the
-# SPI port on the ADC to the Cobbler
 SPICLK = 11
 SPIMISO = 9
 SPIMOSI = 10
@@ -12,31 +10,27 @@ mq7_apin = 0
 mq5_apin=1
 mq5_dpin=27
 
-#port init
 def init():
          GPIO.setwarnings(False)
-         GPIO.cleanup()			#clean up at the end of your script
-         GPIO.setmode(GPIO.BCM)		#to specify whilch pin numbering system
-         #GPIO.setmode(GPIO.BOARD)
-         # set up the SPI interface pins
+         GPIO.cleanup()			
+         GPIO.setmode(GPIO.BCM)		
          GPIO.setup(SPIMOSI, GPIO.OUT)
          GPIO.setup(SPIMISO, GPIO.IN)
          GPIO.setup(SPICLK, GPIO.OUT)
          GPIO.setup(SPICS, GPIO.OUT)
          GPIO.setup(mq7_dpin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
          GPIO.setup(mq5_dpin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-#read SPI data from MCP3008(or MCP3204) chip,8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         if ((adcnum > 7) or (adcnum < 0)):
                 return -1
         GPIO.output(cspin, True)	
 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
+        GPIO.output(clockpin, False)  
+        GPIO.output(cspin, False)     
 
         commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
+        commandout |= 0x18 
+        commandout <<= 3    
         for i in range(5):
                 if (commandout & 0x80):
                         GPIO.output(mosipin, True)
@@ -47,7 +41,7 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
                 GPIO.output(clockpin, False)
 
         adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
+       
         for i in range(12):
                 GPIO.output(clockpin, True)
                 GPIO.output(clockpin, False)
@@ -57,13 +51,11 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
 
         GPIO.output(cspin, True)
         
-        adcout >>= 1       # first bit is 'null' so drop it
+        adcout >>= 1      
         return adcout
 #main ioop
 def main():
          init()
-         print"please wait..."
-         time.sleep(20)
          while True:
                   COlevel=readadc(mq7_apin, SPICLK, SPIMOSI, SPIMISO, SPICS)
                   LPGlevel=readadc(mq5_apin,SPICLK,SPIMOSI,SPIMISO,SPICS)
