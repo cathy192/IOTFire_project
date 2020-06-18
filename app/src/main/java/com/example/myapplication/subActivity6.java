f0f1ff69c488b6e3cff8557a585f6bd5e764d609
cathy192 <korea@2047>
2020-06-19 02:25:03 +0900

    make configue, Video UI

--- app/src/main/java/com/example/myapplication/subActivity6.java
+++ app/src/main/java/com/example/myapplication/subActivity6.java
@@ -7,6 +7,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+
+import com.kyleduo.switchbutton.SwitchButton;
 
 public class subActivity6 extends AppCompatActivity {
     Button stop,start;
@@ -14,8 +17,20 @@ public class subActivity6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub6);
-        start=(Button)findViewById(R.id.startButton);
-        stop=(Button)findViewById(R.id.stopButton);
+        //   start=(Button)findViewById(R.id.startButton);
+        //  stop=(Button)findViewById(R.id.stopButton);
+        SwitchButton switchButton = (SwitchButton) findViewById(R.id.sb_use_listener);
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    startService();
+                } else {
+                    stopService();
+                }
+            }
+        });
+/*
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,7 +42,7 @@ public class subActivity6 extends AppCompatActivity {
             public void onClick(View v) {
                 stopService();
             }
-        });
+        });*/
     }
     public void startService() {
         Intent serviceIntent = new Intent(this, MyService.class);
